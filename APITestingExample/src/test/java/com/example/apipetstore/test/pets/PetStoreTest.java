343953a40d256cf628f50f0f53503bf2487079ab
luisaferco <luisa.colorado.e@gmail.com>
2022-09-14 17:50:45 -0500

    new Test added and a new description of test suite

--- APITestingExample/src/test/java/com/example/apipetstore/test/pets/PetStoreTest.java
+++ APITestingExample/src/test/java/com/example/apipetstore/test/pets/PetStoreTest.java
@@ -1,55 +1,74 @@
 package com.example.apipetstore.test.pets;
 
+import com.example.apipetstore.data.DataFactory;
+import com.example.apipetstore.models.Pet;
+import com.example.apipetstore.models.TagsItem;
+import com.example.apipetstore.models.responses.PetResponse;
+import com.example.apipetstore.steps.PetSteps;
+import java.util.List;
+
+import net.serenitybdd.core.Serenity;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.Title;
+import net.thucydides.core.annotations.WithTag;
+import net.thucydides.core.annotations.WithTags;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import com.example.apipetstore.data.DataFactory;
-import com.example.apipetstore.models.Pet;
-import com.example.apipetstore.steps.PetSteps;
-
-import net.serenitybdd.junit.runners.SerenityRunner;
-import net.thucydides.core.annotations.Steps;
-import net.thucydides.core.annotations.WithTag;
-import net.thucydides.core.annotations.WithTags;
-
 @RunWith(SerenityRunner.class)
-@WithTags({
-        @WithTag("PageObjects pattern"),
-        @WithTag("version:RELEASE-1"),
-})
+@WithTags({ @WithTag("Everything about your Pets"),
+            @WithTag("version:RELEASE-1") })
 public class PetStoreTest {
 
-    @Steps
-    PetSteps vendor;
+  @Steps
+  PetSteps vendor;
 
-    DataFactory dataFactory;
+  DataFactory dataFactory;
 
-    @Before
-    public void setData(){
-       dataFactory = DataFactory.generateData();
-    }
+  @Before
+  public void setData() {
+    dataFactory = DataFactory.generateData();
+  }
 
-    @Test
-    public void addNewPetWithExistingID() {
-        Pet pet = dataFactory.pet();
-        vendor.createANewPet(pet);
-        vendor.shouldSeeANewIdPet(pet.getId());
-        vendor.findPetById(pet.getId());
-        vendor.shouldSeePet(pet);
-    }
+  @Title("Add a new pet adding usign an assigned ID")
+  @Test
+  public void addNewPetWithAssignedID() {
+    Pet pet = dataFactory.pet();
+    Serenity.recordReportData()
+      .withTitle("ID generated ")
+      .andContents(Integer.toString(pet.getId()));
+    vendor.createANewPet(pet);
+    vendor.shouldSeeANewIdPet(pet.getId());
+    vendor.findPetById(pet.getId());
+    vendor.shouldSeePet(pet);
+  }
 
-    @Test
-    public void addNewPetWithNoID() {
-        Pet pet = dataFactory.genericPet();
-        vendor.createANewPet(pet);
-        long id = vendor.getPetID();
-        vendor.shouldSeeNewPetWithIdAssigned();
-        vendor.findPetById(id);
-        vendor.shouldSeePet(pet);
+  @Title("Add a new Pet with no ID assigned")
+  @Test
+  public void addNewPetWithNoID() {
+    Pet pet = dataFactory.genericPet();
+    vendor.createANewPet(pet);
+    long id = vendor.getPetID();
+    vendor.shouldSeeNewPetWithIdAssigned();
+    vendor.findPetById(id);
+    vendor.shouldSeePet(pet);
+  }
 
-    }
+  @Title("Update a pet adding a new tag")
+  @Test
+  public void updateAPet() {
+    Pet pet = dataFactory.genericPet();
+    vendor.createANewPet(pet);
+    PetResponse petResponse = vendor.findPetById(vendor.getPetID());
 
+    TagsItem newTag = dataFactory.generateATag(1, "vaccine");
+    List<TagsItem> updatedTags = petResponse.getTags();
+    updatedTags.add(newTag);
+    petResponse.setTags(updatedTags);
 
-
+    vendor.updateAPet(petResponse);
+    vendor.shouldGetAPetUpdated(petResponse);
+  }
 }
