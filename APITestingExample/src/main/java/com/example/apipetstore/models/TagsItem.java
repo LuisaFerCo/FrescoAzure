4686b93211f52c8f1e5828e2072eddf44438c10b
luisaferco <luisa.colorado.e@gmail.com>
2023-01-04 20:25:24 -0500

    unued import removed

--- APITestingExample/src/main/java/com/example/apipetstore/models/TagsItem.java
+++ APITestingExample/src/main/java/com/example/apipetstore/models/TagsItem.java
@@ -3,7 +3,6 @@ package com.example.apipetstore.models;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
